4ae51ea76704b938fd5cc99226aa930de8544dfc
Kerem Üllenoğlu <kerem.ullen@pm.me>
2023-07-02 14:16:17 +0300

    tests better implementations

--- apis/tests.py
+++ apis/tests.py
@@ -18,9 +18,9 @@ class AccountTests(APITestCase):
             "password": "testt1234",
         }
         response = self.client.post(url, data, format="json")
-        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
-        self.assertEquals(data["username"], response.data["username"])
-        self.assertEquals(1, User.objects.count())
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(data["username"], response.data["username"])
+        self.assertEqual(1, User.objects.count())
 
 
 class TodosTests(APITestCase):
@@ -42,26 +42,26 @@ class TodosTests(APITestCase):
         Ensure we can't get any information without authentication
         """
         response = self.client.get(self.url_todos)
-        self.assertEquals(status.HTTP_401_UNAUTHORIZED, response.status_code)
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_create_todo_un_authenticated(self):
         """
         Ensure no one can create a todo without authentication
         """
         response = self.client.post(self.url_todos, self.test_todo, format="json")
-        self.assertEquals(status.HTTP_401_UNAUTHORIZED, response.status_code)
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_create_todo_authenticated(self):
         """
         Ensure we can create a todo
         """
         response = self.client.post(self.url_todos, self.test_todo, headers=self.headers, format="json")
-        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
-        self.assertEquals(self.test_todo["title"], response.data["title"])
-        self.assertEquals(self.test_todo["description"], response.data["description"])
-        self.assertEquals(False, response.data["done"])
-        self.assertEquals(self.user.id, response.data["owner"])
-        self.assertEquals(1, Todo.objects.count())
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(self.test_todo["title"], response.data["title"])
+        self.assertEqual(self.test_todo["description"], response.data["description"])
+        self.assertFalse(response.data["done"])
+        self.assertEqual(self.user.id, response.data["owner"])
+        self.assertEqual(1, Todo.objects.count())
 
 
 class TodoTests(APITestCase):
@@ -82,27 +82,27 @@ class TodoTests(APITestCase):
         """
         response = self.client.get(self.url_todos, headers=self.headers)
         data = response.data
-        self.assertEquals(1, len(data))
+        self.assertEqual(1, len(data))
         response_todo = data[0]
-        self.assertEquals(self.test_todo_object.id, response_todo["id"])
-        self.assertEquals(self.test_todo_object.title, response_todo["title"])
-        self.assertEquals(self.user.id, response_todo["owner"])
+        self.assertEqual(self.test_todo_object.id, response_todo["id"])
+        self.assertEqual(self.test_todo_object.title, response_todo["title"])
+        self.assertEqual(self.user.id, response_todo["owner"])
 
     def test_view_todo_un_authenticated(self):
         """
         Ensure no one can view the todo without authentication
         """
         response = self.client.get(self.url_todo)
-        self.assertEquals(status.HTTP_401_UNAUTHORIZED, response.status_code)
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_view_todo(self):
         """
         Ensure we can get the details of a todo with its id
         """
         response = self.client.get(self.url_todo, headers=self.headers)
-        self.assertEquals(status.HTTP_200_OK, response.status_code)
-        self.assertEquals(self.test_todo_object.id, response.data["id"])
-        self.assertEquals(self.test_todo_object.title, response.data["title"])
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(self.test_todo_object.id, response.data["id"])
+        self.assertEqual(self.test_todo_object.title, response.data["title"])
 
     def test_update_todo(self):
         """
@@ -113,15 +113,15 @@ class TodoTests(APITestCase):
             "done": True
         }
         response = self.client.put(self.url_todo, update, headers=self.headers)
-        self.assertEquals(status.HTTP_200_OK, response.status_code)
-        self.assertEquals(self.test_todo_object.id, response.data["id"])
-        self.assertEquals(self.test_todo_object.title, response.data["title"])
-        self.assertEquals(update["description"], response.data["description"])
-        self.assertEquals(update["done"], response.data["done"])
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(self.test_todo_object.id, response.data["id"])
+        self.assertEqual(self.test_todo_object.title, response.data["title"])
+        self.assertEqual(update["description"], response.data["description"])
+        self.assertEqual(update["done"], response.data["done"])
 
     def test_delete_todo(self):
         """
         Ensure we can delete a todo
         """
         response = self.client.delete(self.url_todo, headers=self.headers)
-        self.assertEquals(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
